87024de1ed4c9aa201ac4fd8d4f67d31e9f20336
Zachary Werner <drummerkyran77@gmail.com>
2016-01-25 09:23:23 -0500

    Reformat

--- twitch-auto-theater.js
+++ twitch-auto-theater.js
@@ -2,41 +2,42 @@
 // @name         Twitch Auto Theater
 // @version      1.0.0
 // @updateURL    https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
-// @downloadURL    https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
+// @downloadURL  https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
 // @match        www.twitch.tv/*
 // ==/UserScript==
-/* jshint -W097 */
+
+/* jshint -W097 jquery:true */
+
 'use strict';
 
-/* jshint jquery:true */
+var observer = new MutationObserver(function (mutations) {
+	mutations.forEach(function (mutation) {
+		if (!mutation.addedNodes) {
+			return;
+		}
 
-var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        if (!mutation.addedNodes) { return; }
-
-        for (var i = 0; i < mutation.addedNodes.length; i++) {
-            // do things to your newly added nodes here
-            var $node = $(mutation.addedNodes[i]).find('button.player-button--theatre');
-            if ($node.length) {
-                $node.trigger('click');
-                observer.disconnect();
-            }
-        }
-    });
+		for (var i = 0; i < mutation.addedNodes.length; i++) {
+			// do things to your newly added nodes here
+			var $node = $(mutation.addedNodes[i]).find('button.player-button--theatre');
+			if ($node.length) {
+				$node.trigger('click');
+				observer.disconnect();
+			}
+		}
+	});
 });
 
-observer.observe(document.body, {
-    childList: true
-    , subtree: true
-    , attributes: false
-    , characterData: false
-});
+function startObserver() {
+	observer.observe(document.body, {
+		childList: true,
+		subtree: true,
+		attributes: false,
+		characterData: false
+	});
+}
 
-window.onpopstate = function(event) {
-    observer.observe(document.body, {
-        childList: true
-        , subtree: true
-        , attributes: false
-        , characterData: false
-    });
+startObserver();
+
+window.onpopstate = function (event) {
+	startObserver();
 };
