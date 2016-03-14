eefa7ffefa842d9777e08155988f82b284738b38
Zach Werner <drummerkyran77@gmail.com>
2016-03-14 19:48:48 -0400

    Switch to Array.some instead of Array.forEach

--- twitch-auto-theater.js
+++ twitch-auto-theater.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitch Auto Theater
-// @version      1.0.3
+// @version      1.0.4
 // @updateURL    https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
 // @downloadURL  https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
 // @match        www.twitch.tv/*
@@ -12,34 +12,34 @@
 'use strict';
 
 var observer = new MutationObserver(function (mutations) {
-	mutations.forEach(function (mutation) {
-		if (!mutation.addedNodes) {
-			return;
-		}
+    mutations.some(function (mutation) {
+        if (!mutation.addedNodes) {
+            return false;
+        }
 
-		for (var i = 0; i < mutation.addedNodes.length; i++) {
-			// do things to your newly added nodes here
-			var $node = $(mutation.addedNodes[i]).find('button.player-button--theatre');
-			if ($node.length) {
-				$node.trigger('click');
-				observer.disconnect();
-				return;
-			}
-		}
-	});
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+            // do things to your newly added nodes here
+            var $node = $(mutation.addedNodes[i]).find('button.player-button--theatre');
+            if ($node.length) {
+                $node.trigger('click');
+                observer.disconnect();
+                return true;
+            }
+        }
+    });
 });
 
 function startObserver() {
-	observer.observe(document.body, {
-		childList: true,
-		subtree: true,
-		attributes: false,
-		characterData: false
-	});
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+        characterData: false
+    });
 }
 
 startObserver();
 
 window.onpopstate = function (event) {
-	startObserver();
+    startObserver();
 };
