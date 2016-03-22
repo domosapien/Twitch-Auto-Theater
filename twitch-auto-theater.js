8b73f5ae688af569fa2784e3bde17b169e70e207
Zachary Werner <drummerkyran77@gmail.com>
2016-03-22 19:01:08 -0400

    Fix state change detection
    Add .jshintrc

--- twitch-auto-theater.js
+++ twitch-auto-theater.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitch Auto Theater
-// @version      1.0.4
+// @version      1.0.5
 // @updateURL    https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
 // @downloadURL  https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
 // @match        www.twitch.tv/*
@@ -12,34 +12,49 @@
 'use strict';
 
 var observer = new MutationObserver(function (mutations) {
-    mutations.some(function (mutation) {
-        if (!mutation.addedNodes) {
-            return false;
-        }
+	mutations.some(function (mutation) {
+		if (!mutation.addedNodes) {
+			return false;
+		}
 
-        for (var i = 0; i < mutation.addedNodes.length; i++) {
-            // do things to your newly added nodes here
-            var $node = $(mutation.addedNodes[i]).find('button.player-button--theatre');
-            if ($node.length) {
-                $node.trigger('click');
-                observer.disconnect();
-                return true;
-            }
-        }
-    });
+		for (var i = 0; i < mutation.addedNodes.length; i++) {
+			// do things to your newly added nodes here
+			var $node = $(mutation.addedNodes[i]).find('button.player-button--theatre');
+			if ($node.length) {
+				$node.trigger('click');
+				observer.disconnect();
+				return true;
+			}
+		}
+	});
 });
 
 function startObserver() {
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false
-    });
+	observer.observe(document.body, {
+		childList: true,
+		subtree: true,
+		attributes: false,
+		characterData: false
+	});
 }
 
 startObserver();
 
-window.onpopstate = function (event) {
-    startObserver();
+(function (history) {
+	var pushState = history.pushState;
+	history.pushState = function (state) {
+		if (typeof history.onpushstate === "function") {
+			history.onpushstate({ state: state });
+		}
+		return pushState.apply(history, arguments);
+	}
+})(window.history);
+
+window.onpopstate = history.onpushstate = function (event) {
+	if (event.state && event.state.path) {
+		var path = event.state.path.split('/');
+		if (path.length === 2 && path[1] !== 'directory') {
+			startObserver();
+		}
+	}
 };
