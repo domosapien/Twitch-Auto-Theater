23ace3a624912dde5bda223ade007840961abc46
Zach Werner <drummerkyran77@gmail.com>
2017-04-14 19:16:46 -0400

    Fix to work with new Twitch changes

--- twitch-auto-theater.js
+++ twitch-auto-theater.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitch Auto Theater
-// @version      1.0.6
+// @version      1.0.7
 // @updateURL    https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
 // @downloadURL  https://raw.githubusercontent.com/domosapien/Twitch-Auto-Theater/master/twitch-auto-theater.js
 // @match        www.twitch.tv/*
@@ -12,58 +12,50 @@
 'use strict';
 
 var observer = new MutationObserver(function (mutations) {
-	mutations.some(function (mutation) {
-		if (!mutation.addedNodes) {
-			return false;
-		}
+    mutations.some(function (mutation) {
+        if (!mutation.addedNodes) {
+            return false;
+        }
 
-		// For whatever reason, the click event gets trigger twice if you do it programmatically.
-		// Check to see if the trigger was called recently and stop any additional triggers.
-		var lastClick,
-			clickHandler = function (event) {
-				if (lastClick && lastClick.getTime() + 100 >= new Date().getTime()) {
-					event.stopImmediatePropagation();
-				}
-				lastClick = new Date();
-			};
-
-		for (var i = 0; i < mutation.addedNodes.length; i++) {
-			var $node = $(mutation.addedNodes[i]).find('button.player-button--theatre');
-			if ($node.length) {
-				$node.unbind('click').bind('click', clickHandler).click();
-				observer.disconnect();
-				return true;
-			}
-		}
-	});
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+            var $node = $(mutation.addedNodes[i]).find('button.player-button--theatre');
+            if ($node.length) {
+                setTimeout(function () {
+                    $node.click();
+                }, 10);
+                observer.disconnect();
+                return true;
+            }
+        }
+    });
 });
 
 function startObserver() {
-	observer.observe(document.body, {
-		childList: true,
-		subtree: true,
-		attributes: false,
-		characterData: false
-	});
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+        characterData: false
+    });
 }
 
 startObserver();
 
 (function (history) {
-	var pushState = history.pushState;
-	history.pushState = function (state) {
-		if (typeof history.onpushstate === "function") {
-			history.onpushstate({ state: state });
-		}
-		return pushState.apply(history, arguments);
-	};
+    var pushState = history.pushState;
+    history.pushState = function (state) {
+        if (typeof history.onpushstate === "function") {
+            history.onpushstate({ state: state });
+        }
+        return pushState.apply(history, arguments);
+    };
 })(window.history);
 
 window.onpopstate = history.onpushstate = function (event) {
-	if (event.state && event.state.path) {
-		var path = event.state.path.split('/');
-		if (path.length === 2 && path[1] !== 'directory') {
-			startObserver();
-		}
-	}
+    if (event.state && event.state.path) {
+        var path = event.state.path.split('/');
+        if (path.length === 2 && path[1] !== 'directory') {
+            startObserver();
+        }
+    }
 };
